473b6237e447d3ada2a430c8cd11dc62da31f735
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-05-28 15:16:27 +0200

    convert array object to string to pass it to joptionpane

--- src/main/java/com/example/sortNumbers.java
+++ src/main/java/com/example/sortNumbers.java
@@ -4,17 +4,15 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import java.io.*;
 
-/**
- * Hello world!
- */
+
 public class sortNumbers {
 
 
 //bubbleAscending method
-    static void bubbleAscendingSort(int numbersArray[], int n){
+    public static void bubbleAscendingSort(int numbersArray[], int n){
         int i, j, temp;
         boolean swapped; 
-        for (i = 0; i < n-1; i++){
+        for (i = 0; i < n; i++){
             swapped = false; 
             for (j = 0; j < n - i - 1; j++){
                 if (numbersArray[j] > numbersArray[j+1]){
@@ -27,28 +25,30 @@ public class sortNumbers {
             if (swapped == false)
             break; 
         }
-
+        
     }
+
+
 
 //bubbleDescending method
-static void bubbleDescendingSort(int numbersArray[], int n){
-    int i, j, temp;
-    boolean swapped; 
-    for (i = 0; i < n-1; i++){
-        swapped = false; 
-        for (j = 0; j < n - i - 1; j++){
-            if (numbersArray[j] < numbersArray[j+1]){
-                temp = numbersArray[j];
-                numbersArray[j] = numbersArray[j+1];
-                numbersArray[j+1]=temp;
-                swapped = true; 
+    public static void bubbleDescendingSort(int numbersArray[], int n){
+        int i, j, temp;
+        boolean swapped; 
+        for (i = 0; i < n; i++){
+            swapped = false; 
+            for (j = 0; j < n - i - 1; j++){
+                if (numbersArray[j] < numbersArray[j+1]){
+                    temp = numbersArray[j];
+                    numbersArray[j] = numbersArray[j+1];
+                    numbersArray[j+1]=temp;
+                    swapped = true; 
+                }
             }
+            if (swapped == false)
+            break; 
         }
-        if (swapped == false)
-        break; 
+        
     }
-
-}
     public static void main(String[] args){
         
         //input enter a list of numbers of your choice
@@ -58,29 +58,48 @@ static void bubbleDescendingSort(int numbersArray[], int n){
         //select ascending or descending order
         String[] options = {"ascending", "descending"};
         
-        int selection = JOptionPane.showOptionDialog(null, "Select one:", "Number Sort App", 0, 0, null, options, options[0]); 
+        int selection = JOptionPane.showOptionDialog(null, "Select one:", "Number Sort App", 
+        JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, 
+        options, options[0]); 
         //selection devuelve 0 o 1
         System.out.println(selection);
 
         //change type from string to list of int. convertedNumbersArray is an object. Create a new integer list and pass each item from convertedNumbersArray, previously splitted, through a for loop, adding them as integers into a new array (convertedNumberList).
-        String[] convertedNumbersArray = userNumbersString.split(",");
+        String[] convertedNumbersArray = userNumbersString.split(", ");
         int[] numbersArray = new int[convertedNumbersArray.length];
+
         
         for (int i = 0; i < convertedNumbersArray.length; i++) {
-        numbersArray[i] = Integer.parseInt(convertedNumbersArray[i]); 
-        int n = numbersArray.length;
-
+        numbersArray[i] = Integer.parseInt(convertedNumbersArray[i]); }
+        
+int n = numbersArray.length;
         if (selection == 0) {
             bubbleAscendingSort(numbersArray, n);
+            System.out.println("print array method");
+            StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < numbersArray.length; i++ ){
+                    stringBuilder.append(numbersArray[i]);
+                    if (i < numbersArray.length){
+                    stringBuilder.append(", ");
+            
+            String arrayAsString = stringBuilder.toString();
+            System.out.println(arrayAsString);
+        JOptionPane.showMessageDialog(null, arrayAsString);
+            
     }
         else if (selection == 1) {
             bubbleDescendingSort(numbersArray, n);
+            System.out.println("print array method");
+        
         }
        
 
     }
         
-        
+        }
+
+    }
+}
 
         //if selection = 0. Call for ascending bubble
 
@@ -94,8 +113,7 @@ static void bubbleDescendingSort(int numbersArray[], int n){
 //create a method too sort in descending order using bubble sorting
 
         
-        } 
-}
+
         
     
 
