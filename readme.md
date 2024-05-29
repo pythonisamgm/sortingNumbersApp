4df8aa5cdf3739293ccda5d8505a5b02aa347d5a
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-05-29 09:24:21 +0200

    update code and readme file

--- readme.md
+++ readme.md
@@ -1,9 +1,51 @@
-This program will prompt users to enter a series of numbers, then allow them to choose whether they want to sort them in ascending or descending order. The sorted list will then be displayed.
+# Java II: Number Sort App
 
-JOptionPane.showInputDialog() only admits strings. 
-Convertir el array a String y luego, una vez lo introduzca el usuario, convertir el String otra vez a array. 
+This is a Java program that allows users to enter a list of numbers, which it then sorts in either ascending or descending order using the Bubble Sort algorithm. The sorted list is then displayed to the user via a dialog box. Additionally, the program replaces the number "4" with "(3+1)" due to cultural reasons.
+
+## Features
+
+- Enter a list of numbers.
+- Choose to sort the list in ascending or descending order.
+- Display the sorted list using a dialog box.
+- Replace occurrences of the number "4" with "(3+1)".
 
 
+## Requirements
+
+- Java Development Kit (JDK) 8 or higher.
+- An IDE or text editor to run the Java program.
+- Swing library (included in JDK) for dialog boxes.
+
+## ## Code Overview
+
+### Main Class: `sortNumbers`
+
+#### Methods
+
+- `public static void bubbleAscSort(int numbersArray[], int n)`
+  - Sorts the array in ascending order using the Bubble Sort algorithm.
+
+- `public static void bubbleDescSort(int numbersArray[], int n)`
+  - Sorts the array in descending order using the Bubble Sort algorithm.
 
 
-convert userNumbersString into list of int: List<Integer> convertedUserNumbers; 
+#### Main Method
+
+1. **Input:**
+   - Prompts the user to enter a list of numbers separated by commas.
+
+2. **Option Selection:**
+   - Prompts the user to choose between ascending and descending order.
+
+3. **Array Conversion:**
+   - Converts the input string into an integer array.
+
+4. **Sorting:**
+   - Calls the appropriate sorting method based on the user's selection.
+
+5. **Display:**
+   - Creates a StringBuilder object to pass each item from the numbersArray to convert it to string.
+   - Replaces occurrences of the number "4" with "(3+1)".
+   - Displays the sorted list using a dialog box.
+
+
--- src/main/java/com/example/sortNumbers.java
+++ src/main/java/com/example/sortNumbers.java
@@ -1,15 +1,10 @@
 package com.example;
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.JOptionPane;
-import java.io.*;
 
 
 public class sortNumbers {
 
-
-//bubbleAscending method
-    public static void bubbleAscendingSort(int numbersArray[], int n){
+    public static void bubbleAscSort(int numbersArray[], int n){
         int i, j, temp;
         boolean swapped; 
         for (i = 0; i < n; i++){
@@ -25,13 +20,9 @@ public class sortNumbers {
             if (swapped == false)
             break; 
         }
-        
     }
 
-
-
-//bubbleDescending method
-    public static void bubbleDescendingSort(int numbersArray[], int n){
+    public static void bubbleDescSort(int numbersArray[], int n){
         int i, j, temp;
         boolean swapped; 
         for (i = 0; i < n; i++){
@@ -47,81 +38,45 @@ public class sortNumbers {
             if (swapped == false)
             break; 
         }
-        
     }
+    
     public static void main(String[] args){
         
-        //input enter a list of numbers of your choice
         String userNumbersString = JOptionPane.showInputDialog( "Enter a list of numbers of your choice: ");
         System.out.println(userNumbersString);
 
-        //select ascending or descending order
         String[] options = {"ascending", "descending"};
         
         int selection = JOptionPane.showOptionDialog(null, "Select one:", "Number Sort App", 
         JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, 
         options, options[0]); 
-        //selection devuelve 0 o 1
-        
 
-        //change type from string to list of int. convertedNumbersArray is an object. Create a new integer list and pass each item from convertedNumbersArray, previously splitted, through a for loop, adding them as integers into a new array (convertedNumberList).
         String[] convertedNumbersArray = userNumbersString.split(", ");
         int[] numbersArray = new int[convertedNumbersArray.length];
 
-        
         for (int i = 0; i < convertedNumbersArray.length; i++) {
         numbersArray[i] = Integer.parseInt(convertedNumbersArray[i]); }
         
         int n = numbersArray.length;
-        if (selection == 0) 
-            {
-            bubbleAscendingSort(numbersArray, n);
-            
+        if (selection == 0) {
+            bubbleAscSort(numbersArray, n);
             }
-        else if (selection == 1) 
-            {
-            bubbleDescendingSort(numbersArray, n);
-
+        else if (selection == 1)  {
+            bubbleDescSort(numbersArray, n);
             }
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < numbersArray.length; i++ ){
-                stringBuilder.append(numbersArray[i]);
-                    if (i < numbersArray.length)
-                        {
-                        stringBuilder.append(", ");
-                        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < numbersArray.length; i++ ){
+            stringBuilder.append(numbersArray[i]);
+                if (i < numbersArray.length)
+                    {
+                    stringBuilder.append(", ");
                     }
-            String arrayAsString = stringBuilder.toString();
-            String replacedString = arrayAsString.replace("4", "(3+1)");
+                }
+        String arrayAsString = stringBuilder.toString();
+        String replacedString = arrayAsString.replace("4", "(3+1)");
             
-            JOptionPane.showMessageDialog(null, replacedString);
-       
-            }
+        JOptionPane.showMessageDialog(null, replacedString);  
+    } 
+}
 
-    }
-
-        //if selection = 0. Call for ascending bubble
-
-        //if (selection == 0);{
-
-
-
-//if selection = 1. Call for descending bubble
-//create a method to sort in ascending order using bubble sorting
-
-//create a method too sort in descending order using bubble sorting
-
-        
-
-        
-    
-
-
-
-
-
-
-//return sorted list 
-
-//change number 4 for 3+1
 
